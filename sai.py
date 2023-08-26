d13dee023083b76980fd1498ccc93ad05c30a2e9
sri krishna sai <patnalasrikrishnasai@gmail.com>
2023-08-26 10:41:25 +0530

    Updated content in sai.py

--- sai.py
+++ sai.py
@@ -1,1 +1,10 @@
-print("helo")
+def is_palindrome(input_string):
+    cleaned_string = ''.join(filter(str.isalnum, input_string)).lower()
+    return cleaned_string == cleaned_string[::-1]
+
+# Test the function
+input_string = input("Enter a string: ")
+if is_palindrome(input_string):
+    print("It's a palindrome!")
+else:
+    print("It's not a palindrome.")
